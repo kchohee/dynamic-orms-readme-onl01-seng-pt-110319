7297505c69333679b3ffacf2824aae89bf385246
Marie (Cho) Hughes <kchohee0630@gmail.com>
2020-02-02 23:39:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,9 +56,8 @@ end
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
-    DB[:conn].execute(sql)
-  end
+  DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", [name])
+end
 
 end
 
